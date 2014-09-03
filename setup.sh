30f27309f32a1f82a695d96948261a6a250de278
Alex Eveleth <eveleth.alex@gmail.com>
2014-09-02 22:19:08 -0700

    updated setup.sh for inputrc

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ CUR_FILE=`readlink -e $0`
 CUR_DIR=`dirname ${CUR_FILE}`
 ln -s "${CUR_DIR}/bash/bashrc" ~/.bashrc
 ln -s "${CUR_DIR}/bash/aliases" ~/.bash_aliases
+ln -s "${CUR_DIR}/inputrc" ~/.inputrc
 ln -s "${CUR_DIR}/vim" ~/.vim
 ln -s "${CUR_DIR}/vim/vimrc" ~/.vimrc
 ln -s "${CUR_DIR}/redshift.conf" ~/.config/redshift.conf
